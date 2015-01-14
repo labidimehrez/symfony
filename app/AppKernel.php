d2b179140ff98f8830671bab5e9998812cd5e34b
MehezLabidi <mehrez.labidi@esprit.tn>
2015-01-14 01:33:11 +0000

    fosuser

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -22,6 +22,7 @@ class AppKernel extends Kernel
             new MyApp\ArticleBundle\MyAppArticleBundle(),
             new Ivory\CKEditorBundle\IvoryCKEditorBundle(),
             new Twitter\BootstrapBundle\TwitterBootstrapBundle(),
+            new FOS\UserBundle\FOSUserBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
--- src/MyApp/UserBundle/MyAppUserBundle.php
+++ src/MyApp/UserBundle/MyAppUserBundle.php
@@ -6,4 +6,9 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class MyAppUserBundle extends Bundle
 {
+    public function getParent( )
+    {
+        return'FOSUserBundle';
+    }
 }
+
