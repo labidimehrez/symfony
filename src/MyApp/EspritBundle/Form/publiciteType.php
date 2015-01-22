b65ec1b521ee3785ee3578896980919ee471e5dc
MehezLabidi <mehrez.labidi@esprit.tn>
2015-01-22 11:05:32 +0000

    ckeditor mrigel Signed-off-by: MehezLabidi <mehrez.labidi@esprit.tn>

--- src/MyApp/EspritBundle/Form/publiciteType.php
+++ src/MyApp/EspritBundle/Form/publiciteType.php
@@ -6,25 +6,29 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
-class publiciteType extends AbstractType
-{
-        /**
+class publiciteType extends AbstractType {
+
+    /**
      * @param FormBuilderInterface $builder
      * @param array $options
      */
-    public function buildForm(FormBuilderInterface $builder, array $options)
-    {
+    public function buildForm(FormBuilderInterface $builder, array $options) {
         $builder
-            ->add('position')
-           ->add('image', 'ckeditor', array('required' => false))
-     ;   
+                ->add('position')
+                ->add('image', 'ckeditor', array(
+                    'plugins' => array(
+                        'wordcount' => array(
+                            'path' => '/bundles/myappespritbundle/ckeditor/',
+                            'filename' => 'plugin.js',
+                        ),
+            )))
+        ;
     }
-    
+
     /**
      * @param OptionsResolverInterface $resolver
      */
-    public function setDefaultOptions(OptionsResolverInterface $resolver)
-    {
+    public function setDefaultOptions(OptionsResolverInterface $resolver) {
         $resolver->setDefaults(array(
             'data_class' => 'MyApp\EspritBundle\Entity\publicite'
         ));
@@ -33,8 +37,8 @@ class publiciteType extends AbstractType
     /**
      * @return string
      */
-    public function getName()
-    {
+    public function getName() {
         return 'myapp_espritbundle_publicite';
     }
+
 }
--- src/MyApp/UserBundle/Controller/UserController.php
+++ src/MyApp/UserBundle/Controller/UserController.php
@@ -1,19 +1,43 @@
 <?php
 
 namespace MyApp\UserBundle\Controller;
+
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class UserController extends Controller {
 
-    public function showAction() {
+    public function showAction( Request $request) {
         $em = $this->getDoctrine()->getManager();
 
         $user = $em->getRepository('MyAppUserBundle:user')->findAll();
 
-        return $this->render('MyAppUserBundle:user:show.html.twig', array(
-                    'user' => $user,
-        ));
+        $form = $this->createFormBuilder($user)
+
+
+//            ->add('username', 'text')
+                ->add('roles', 'collection', array(
+                    'type' => 'choice',
+                    'options' => array(
+                        'label' => false, /* Ajoutez cette ligne */
+                        'choices' => array(
+                            'ROLE_ADMIN' => 'Admin',
+                            'ROLE_SUPER_ADMIN' => 'Superadmin',
+                            'ROLE_SUPERSOL' => 'Supersol',
+                            'ROLE_EDITOR' => 'Editor',
+                            'ROLE_USER' => 'User',
+                ))))
+                ->getForm();
+
+        $form->handleRequest($request);
+
+
+        if ($form->isValid()) {
+            $em->flush();
+            return $this->redirect($this->generateUrl('my_app_user_show'));
+        }
+
+        return $this->render('MyAppUserBundle:user:show.html.twig', array('form' => $form->createView()));
     }
 
     public function deleteAction($id) {
@@ -30,45 +54,42 @@ class UserController extends Controller {
         return $this->redirect($this->generateUrl('my_app_user_show'));
     }
 
-   public function editAction($id, Request $request) {
+    public function editAction($id, Request $request) {
 
-    $em = $this->getDoctrine()->getManager();
-    $user = $em->getRepository('MyAppUserBundle:user')->find($id);
-    if (!$user) {
-      throw $this->createNotFoundException(
-              'no Utilisateur found for id ' . $id
-      );
-    }
-    
-    $form = $this->createFormBuilder($user)
-        
-        
+        $em = $this->getDoctrine()->getManager();
+        $user = $em->getRepository('MyAppUserBundle:user')->find($id);
+        if (!$user) {
+            throw $this->createNotFoundException(
+                    'no Utilisateur found for id ' . $id
+            );
+        }
+
+        $form = $this->createFormBuilder($user)
+
+
 //            ->add('username', 'text')
-            ->add('roles', 'collection', array(
-                      
-                   'type' => 'choice',
-                   'options' => array(
-                    
-                       'label' => false, /* Ajoutez cette ligne */                    
-                       'choices' => array(
-                           'ROLE_ADMIN' => 'Admin',
-                           'ROLE_SUPER_ADMIN' => 'Superadmin',
-                           'ROLE_SUPERSOL' => 'Supersol',
-                           'ROLE_EDITOR' => 'Editor',
-                           'ROLE_USER' => 'User',
-                       ) ) ) )
-        
-        ->getForm();
+                ->add('roles', 'collection', array(
+                    'type' => 'choice',
+                    'options' => array(
+                        'label' => false, /* Ajoutez cette ligne */
+                        'choices' => array(
+                            'ROLE_ADMIN' => 'Admin',
+                            'ROLE_SUPER_ADMIN' => 'Superadmin',
+                            'ROLE_SUPERSOL' => 'Supersol',
+                            'ROLE_EDITOR' => 'Editor',
+                            'ROLE_USER' => 'User',
+                ))))
+                ->getForm();
 
-    $form->handleRequest($request);
- 
+        $form->handleRequest($request);
 
-    if ($form->isValid()) {
-        $em->flush();
-       return $this->redirect($this->generateUrl('my_app_user_show'));
+
+        if ($form->isValid()) {
+            $em->flush();
+            return $this->redirect($this->generateUrl('my_app_user_show'));
+        }
+
+        return $this->render('MyAppUserBundle:user:edit.html.twig', array('form' => $form->createView()));
     }
-                            
-       return $this->render('MyAppUserBundle:user:edit.html.twig',array('form'=>$form->createView())); 
-    }
- 
+
 }
