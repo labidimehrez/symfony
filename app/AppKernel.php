ed511da3cb8308b1a7e4dc7dfeef126901417d12
MehezLabidi <mehrez.labidi@esprit.tn>
2015-02-09 01:15:27 +0000

    ajout de TwigExtensionsBundle Signed-off-by: MehezLabidi <mehrez.labidi@esprit.tn>

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -28,6 +28,7 @@ class AppKernel extends Kernel
             new FOS\CommentBundle\FOSCommentBundle(),
             new JMS\SerializerBundle\JMSSerializerBundle($this),
             new Knp\Bundle\TimeBundle\KnpTimeBundle(),
+            new Craue\TwigExtensionsBundle\CraueTwigExtensionsBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
