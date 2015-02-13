927955183ea677ef6a99f48f1c92c83e987b15f0
MehezLabidi <mehrez.labidi@esprit.tn>
2015-02-13 12:43:59 +0000

    les pub personnalisés Signed-off-by: MehezLabidi <mehrez.labidi@esprit.tn>

--- src/MyApp/EspritBundle/Controller/publiciteController.php
+++ src/MyApp/EspritBundle/Controller/publiciteController.php
@@ -2,7 +2,6 @@
 
 namespace MyApp\EspritBundle\Controller;
 
- 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use MyApp\EspritBundle\Form\publiciteType;
 use MyApp\EspritBundle\Entity\publicite;
@@ -10,9 +9,8 @@ use MyApp\UserBundle\Entity\User;
 
 class publiciteController extends Controller {
 
- 
     public function addAction() {
-        /********* ajout de nouveau publicite  *******/
+        /*         * ******* ajout de nouveau publicite  ****** */
         $publicite = new publicite();
         $form = $this->createForm(new publiciteType(), $publicite);
         $request = $this->getRequest();
@@ -25,28 +23,36 @@ class publiciteController extends Controller {
                 $em = $this->getDoctrine()->getManager();
                 $em->persist($publicite);
                 $em->flush();
-                
+
                 return $this->redirect($this->generateUrl('my_app_esprit_publicite_add'));
             }
         }
         return $this->render('MyAppEspritBundle:publicite:add.html.twig', array('form' => $form->createView()));
     }
 
- 
+    public function showminpubAction() {
+        $em = $this->getDoctrine()->getManager();
+        /*         * ****  recuperer les pub les plus importants *************************** */
+        $publicite = $em->getRepository('MyAppEspritBundle:publicite')->getMinPub();
+        return $this->render('MyAppEspritBundle:publicite:show.html.twig', array(
+                    'publicite' => $publicite,
+        ));
+    }
+
     public function showAction() {
 
         $em = $this->getDoctrine()->getManager();
 
-        /*  si aucun user dans la base on crée un par defaut  **/
+        /*  si aucun user dans la base on crée un par defaut  * */
         $user = $em->getRepository('MyAppUserBundle:user')->findAll();
         if (!$user) {
-            /*** clear de la table pour revenir à id=1 ****/
+            /*             * * clear de la table pour revenir à id=1 *** */
             $sql = 'TRUNCATE TABLE user;';
             $connection = $em->getConnection();
             $stmt = $connection->prepare($sql);
             $stmt->execute();
             $stmt->closeCursor();
-            /*** default new user ***/
+            /*             * * default new user ** */
             $user = new User();
             $user->setUsername("root");
             $user->setSexe("m");
@@ -66,8 +72,8 @@ class publiciteController extends Controller {
             $em1->persist($user);
             $em1->flush();
         }
-        
-        /******  tous les pub de la base ****************************/
+
+        /*         * ****  tous les pub de la base *************************** */
         $publicite = $em->getRepository('MyAppEspritBundle:publicite')->getAllPub();
         return $this->render('MyAppEspritBundle:publicite:show.html.twig', array(
                     'publicite' => $publicite,
@@ -75,7 +81,7 @@ class publiciteController extends Controller {
     }
 
     public function deleteAction($id) {
-        /****** delete d'une pub si il existe deja ****/
+        /*         * **** delete d'une pub si il existe deja *** */
         $em = $this->getDoctrine()->getManager();
         $publicite = $em->getRepository('MyAppEspritBundle:publicite')->find($id);
         if (!$publicite) {
@@ -86,9 +92,9 @@ class publiciteController extends Controller {
 
         return $this->redirect($this->generateUrl('my_app_esprit_publicite_manage'));
     }
- 
-    public function manageAction() {   
-        /******  ajout de nouveau pub  *****/
+
+    public function manageAction() {
+        /*         * ****  ajout de nouveau pub  **** */
         $em = $this->getDoctrine()->getManager();
         $publicite = new publicite();
         $form = $this->createForm(new publiciteType, $publicite);
@@ -104,16 +110,15 @@ class publiciteController extends Controller {
                 return $this->redirect($this->generateUrl('my_app_esprit_publicite_manage'));
             }
         }
-       /**********  recuperation des pub ordonnés  ********************/
+        /*         * ********  recuperation des pub ordonnés  ******************* */
         $publicite2 = $em->getRepository('MyAppEspritBundle:publicite')->findBy(array(), array('position' => 'asc'), 100, 0);
 
         return $this->render('MyAppEspritBundle:publicite:manage.html.twig', array('form' => $form->createView(),
                     'publicite2' => $publicite2, 'publicite' => $publicite));
     }
 
- 
     public function deleteallAction() {
-        /*********  delete all Pubs *****/
+        /*         * *******  delete all Pubs **** */
         $em = $this->getDoctrine()->getManager();
         $sql = 'TRUNCATE TABLE publicite;';
         $connection = $em->getConnection();
--- src/MyApp/EspritBundle/Repository/publiciteRepository.php
+++ src/MyApp/EspritBundle/Repository/publiciteRepository.php
@@ -8,7 +8,16 @@ class publiciteRepository extends EntityRepository
    public function getAllPub()
     {
         return $this->getEntityManager()
-            ->createQuery('SELECT p FROM MyAppEspritBundle:publicite p  ')
+            ->createQuery('SELECT  p FROM MyAppEspritBundle:publicite p  ')
+            ->getResult();
+            
+    }
+    
+      public function getMinPub()
+    {
+        return $this->getEntityManager()
+            ->createQuery('SELECT  p FROM MyAppEspritBundle:publicite p ORDER BY p.position ASC ')          
+            ->setMaxResults(4)   
             ->getResult();
             
     }
