177ea2a869bc1ad198abef76da906c43f2fe89bd
MehezLabidi <mehrez.labidi@esprit.tn>
2015-02-27 18:37:05 +0000

    /index.php Signed-off-by: MehezLabidi <mehrez.labidi@esprit.tn>

--- src/MyApp/EspritBundle/Repository/publiciteRepository.php
+++ src/MyApp/EspritBundle/Repository/publiciteRepository.php
@@ -14,15 +14,16 @@ class publiciteRepository extends EntityRepository {
 
     public function getextPub() {
         return $this->getEntityManager()
-                        ->createQuery('SELECT  p FROM MyAppEspritBundle:publicite p WHERE  p.position in (0,1,6,7) ')
+                        ->createQuery('SELECT  p FROM MyAppEspritBundle:publicite p WHERE'
+                                . '  p.position in (0,1,6,7) ')
                         ->getResult();
     }
 
     public function getintPub() {
         return $this->getEntityManager()
- ->createQuery('SELECT  p FROM MyAppEspritBundle:publicite p WHERE '
-         . ' p.position  in (8,9,10,11,12,13,14,15,16,17,18,19,20,21)  ')
-  ->getResult();
+                        ->createQuery('SELECT  p FROM MyAppEspritBundle:publicite p WHERE '
+                                . ' p.position  in (8,9,10,11,12,13,14,15,16,17,18,19,20,21)  ')
+                        ->getResult();
     }
 
 }
