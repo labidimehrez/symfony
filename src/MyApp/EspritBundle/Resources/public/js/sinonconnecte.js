600ccf059b081fb766146791b819da9093068ac8
MehezLabidi <mehrez.labidi@esprit.tn>
2015-03-23 14:45:42 +0000

    si sol editor ou proprietaire can edit/delete Signed-off-by: MehezLabidi <mehrez.labidi@esprit.tn>

---
+++ src/MyApp/EspritBundle/Resources/public/js/sinonconnecte.js
@@ -0,0 +1,26 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+       $(document).ready(function() {
+                $(".flip").click(function()
+                {
+                    $(".panel3").slideToggle("slow");
+                });
+
+
+
+                $(".flip2").click(function()
+                {
+                    $(".panel3").slideToggle("slow");
+                });
+
+
+                $(".flip3").click(function()
+                {
+                    $(".panel3").fadeOut();
+                });
+
+            });
+
