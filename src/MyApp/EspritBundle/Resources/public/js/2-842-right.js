4ec8a31b98e3abd660120f7fac0ad8672b114919
MehezLabidi <mehrez.labidi@esprit.tn>
2015-04-01 11:26:19 +0000

    reglage de scrollTop Signed-off-by: MehezLabidi <mehrez.labidi@esprit.tn>

---
+++ src/MyApp/EspritBundle/Resources/public/js/2-842-right.js
@@ -0,0 +1,22 @@
+var html_842 =
+"<style type=\"text/css\">#navigation_up_down_842 \n\
+.navigation_up_down_842, #navigation_up_down_842 \n\
+.navigation_up_down_842 div{	padding:0px;	margin:0px;	border:0px;}\n\
+#navigation_up_down_842 .navigation_up_down\n\
+{	margin:auto;	width:30px;	height:60px;	position:fixed;	bottom:5px; top:100px;	right:5px;  z-index: 10;}\n\
+#navigation_up_down_842 .navigation_up_down \n\
+.navigation_up_down_up{	margin:auto;	display:block;	width:30px;	height:30px;	cursor:pointer;\n\
+background:url(\'http://services.supportduweb.com/navigation_up_down/styles/images/s2.png\');\n\
+background-position: 0px 0px;}#navigation_up_down_842 .navigation_up_down \n\
+.navigation_up_down_up:hover{	background-position: -30px 0px;}#navigation_up_down_842 \n\
+.navigation_up_down .navigation_up_down_up:active{	background-position: -60px 0px;}\n\
+#navigation_up_down_842 .navigation_up_down \n\
+.navigation_up_down_down{	margin:auto;	display:block;	width:30px;	height:30px;	cursor:pointer;\n\
+background:url(\'http://services.supportduweb.com/navigation_up_down/styles/images/s2.png\');	\n\
+background-position: 0px -30px;}#navigation_up_down_842 .navigation_up_down \n\
+.navigation_up_down_down:hover{	background-position: -30px -30px;}\n\
+#navigation_up_down_842 .navigation_up_down \n\
+.navigation_up_down_down:active{background-position: -60px -30px;}</style><div class=\"navigation_up_down\">	\n\
+<div class=\"navigation_up_down_up\" onclick=\"window.scrollTo(0,1);window.scrollTo(0,0);\"></div>	\n\
+<div class=\"navigation_up_down_down\" onclick=\"window.scrollTo(document.body.scrollWidth,document.body.clientHeight*100000);\"></div></div>";
+document.getElementById('navigation_up_down_842').innerHTML=html_842;
