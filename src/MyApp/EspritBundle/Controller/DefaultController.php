faa442713c9a798167d954ebd05f03b189c7cc15
MehezLabidi <mehrez.labidi@esprit.tn>
2015-05-05 18:38:23 +0000

    traduction Signed-off-by: MehezLabidi <mehrez.labidi@esprit.tn>

--- src/MyApp/EspritBundle/Controller/DefaultController.php
+++ src/MyApp/EspritBundle/Controller/DefaultController.php
@@ -1,8 +1,10 @@
 <?php
 
 namespace MyApp\EspritBundle\Controller;
+
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\JsonResponse;
- 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller {
@@ -31,16 +33,23 @@ class DefaultController extends Controller {
         return $this->render('MyAppEspritBundle:BackOffice:administration.html.twig');
     }
 
-        public function testAction() {
-         $em = $this->get('doctrine.orm.entity_manager');
-        /*         * ********* **    recuperation de tout les menus  *********** */
-        $menu = $em->getRepository('MyAppEspritBundle:menu')->getAllMenu();
-        return $this->render('MyAppEspritBundle:Default:test.html.twig', array('menu' => $menu));
-                    
-        
+    public function testAction() {
+        $request = $this->getRequest();
+        echo $request->getLocale();
+        die();
+        return $this->render('MyAppEspritBundle:Default:test.html.twig');
     }
-    
-    
+
+    public function translationAction() {
+
+        $this->getRequest()->setLocale('en_US');
+        $this->getRequest()->setDefaultLocale('en');
+       /* $x=$this->getRequest();
+       var_dump($x);exit;*/
+
+        return $this->render('MyAppEspritBundle:Default:translation.html.twig');
+    }
+
     public function routeAction() {
         $request = $this->container->get('request');
         $routeName = $request->get('_route');
@@ -49,4 +58,7 @@ class DefaultController extends Controller {
                     'routeName' => $routeName
         ));
     }
+
+ 
+
 }
